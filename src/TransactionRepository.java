fc2428d22c0f105e8261746ebb251a454fa05e02
mathieu <hfz5hw9qd8@privaterelay.appleid.com>
2026-08-16 19:37:51 +0200

    Ajoute la recherche des transactions par compte

--- src/TransactionRepository.java
+++ src/TransactionRepository.java
@@ -3,13 +3,26 @@ import java.util.List;
 
 public class TransactionRepository {
 
-    private List<Transaction> transactions = new ArrayList<>();
+    private final List<Transaction> transactions = new ArrayList<>();
 
     public void sauvegarder(Transaction transaction) {
         transactions.add(transaction);
     }
 
     public List<Transaction> trouverToutes() {
-        return transactions;
+        return List.copyOf(transactions);
+    }
+
+    public List<Transaction> trouverParCompte(Compte compte) {
+        List<Transaction> resultat = new ArrayList<>();
+
+        for (Transaction transaction : transactions) {
+            if (transaction.getSource() == compte
+                    || transaction.getDestinataire() == compte) {
+                resultat.add(transaction);
+            }
+        }
+
+        return List.copyOf(resultat);
     }
 }
