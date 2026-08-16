03b02bdcf906f7443e2d226b34ddf83f23236299
mathieu <hfz5hw9qd8@privaterelay.appleid.com>
2026-08-16 19:51:44 +0200

    Ajoute le premier endpoint REST /api/hello

---
+++ src/main/java/com/bank/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.bank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Bienvenue dans votre système bancaire !";
+    }
+}
